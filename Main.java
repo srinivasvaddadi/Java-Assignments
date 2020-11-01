6b20749d0f347dd5afab76a08146c2be398413c6
srinivasvaddadi <42258634+srinivasvaddadi@users.noreply.github.com>
2020-11-01 23:51:53 +0530

    Assignment6(problem 1) submitted

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.Assignment6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.getDetails();
+		emp.display();
+		System.out.println();
+		emp.doWork();
+
+	}
+
+}
