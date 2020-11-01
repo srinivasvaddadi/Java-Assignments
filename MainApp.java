78340a38970fa448b90e165caf23017fd659a559
srinivasvaddadi <42258634+srinivasvaddadi@users.noreply.github.com>
2020-11-02 00:50:33 +0530

    Assignment6(problem 1) Submitted

---
+++ MainApp.java
@@ -0,0 +1,14 @@
+package com.Assignment6;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.getDetails();
+		emp.display();
+		System.out.println();
+		emp.doWork();
+
+	}
+
+}
